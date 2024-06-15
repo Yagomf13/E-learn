32bf02ae5310d98bb64b1a3a8897535b9e5dd8d0
Yago Martín <yagomf13@gmail.com>
2024-06-15 12:48:14 +0200

    Registro done

--- Hackathon/App1/urls.py
+++ Hackathon/App1/urls.py
@@ -3,12 +3,14 @@ from App1.views import IndexView, LoginView, RegisterView, cursos_view, crear_us
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', IndexView),
+    path('', IndexView, name='index'),
     path('login/', LoginView, name='login'),
-    path('register/' , RegisterView, name='register'),
+    path('register/', RegisterView, name='register'),
     path('cursos/', cursos_view, name='cursos'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('crear_usuario/', crear_usuario, name='crear_usuario'),
     path('user/', UserView, name='user'),
     path('logout/', LogoutView, name='logout'),
+    path('crear_usuario/', crear_usuario, name='crear_usuario'),
+    # Agrega la URL para la página de logout utilizando auth_views
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
+    
--- Hackathon/App1/views.py
+++ Hackathon/App1/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Curso
@@ -5,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Curso, Usuarios
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
 def IndexView(request):
     """Página de inicio"""
@@ -56,19 +58,76 @@ def crear_usuario(request):
         contraseña = request.POST.get('contraseña')
         birthday = request.POST.get('birthday')
         
+        # Verifica si ya existe un usuario con el mismo email
+        if User.objects.filter(username=email).exists():
+            messages.error(request, 'El correo ya se esta utilizando.')
+            return render(request, 'register.html', {'email_error': 'El usuario ya existe.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+        
+        for field_name, field_value in [('nombre', nombre), ('apellido', apellido)]:
+            if not field_value or not re.match(r'^[a-zA-Z áéíóúùìòàè]+$', field_value):
+                messages.error(request, f'El {field_name} solo debe contener letras.')
+                return render(request, 'register.html', {'form_error': f'El {field_name} solo debe contener letras.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+        
+        try:
+            birth_date = datetime.strptime(birthday, '%Y-%m-%d').date()
+        except ValueError:
+            messages.error(request, 'Formato de fecha inválido.')
+            return render(request, 'register.html', {'form_error': 'Formato de fecha inválido.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+        
+        # Valida que la fecha de nacimiento sea anterior al día actual y menor de 120 años en el pasado
+        today = datetime.now().date()
+        max_birth_date = today - timedelta(days=365 * 120)
+        
+        if birth_date >= today:
+            messages.error(request, 'La fecha de nacimiento debe ser anterior al día actual.')
+            return render(request, 'register.html', {'form_error': 'La fecha de nacimiento debe ser anterior al día actual.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+
+        if birth_date <= max_birth_date:
+            messages.error(request, 'La fecha de nacimiento debe ser menor de 120 años en el pasado.')
+            return render(request, 'register.html', {'form_error': 'La fecha de nacimiento debe ser menor de 120 años en el pasado.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+
+        # Validación de contraseña segura
+        if len(contraseña) < 8:
+            messages.error(request, 'La contraseña debe tener al menos 8 caracteres.')
+            return render(request, 'register.html', {'form_error': 'La contraseña debe tener al menos 8 caracteres.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+
+        if not re.search(r'[A-Z]', contraseña):
+            messages.error(request, 'La contraseña debe contener al menos una letra mayúscula.')
+            return render(request, 'register.html', {'form_error': 'La contraseña debe contener al menos una letra mayúscula.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+
+        if not re.search(r'[a-z]', contraseña):
+            messages.error(request, 'La contraseña debe contener al menos una letra minúscula.')
+            return render(request, 'register.html', {'form_error': 'La contraseña debe contener al menos una letra minúscula.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+
+        if not re.search(r'\d', contraseña):
+            messages.error(request, 'La contraseña debe contener al menos un número.')
+            return render(request, 'register.html', {'form_error': 'La contraseña debe contener al menos un número.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', contraseña):
+            messages.error(request, 'La contraseña debe contener al menos un caracter especial (!@#$%^&*(),.?":{}|<>).')
+            return render(request, 'register.html', {'form_error': 'La contraseña debe contener al menos un caracter especial.', 'nombre': nombre, 'apellido': apellido, 'email': email, 'birthday': birthday})
+
         # Crea un usuario en el modelo de usuario de Django
         user = User.objects.create_user(username=email, email=email, password=contraseña)
         
         # Crea una instancia de Usuarios asociada a este usuario
-        usuario = Usuarios(user=user, nombre=nombre, apellido=apellido, email=email, contraseña=contraseña,
-                        birthday=birthday)
+        usuario = Usuarios(user=user, nombre=nombre, apellido=apellido, email=email, contraseña=contraseña, birthday=birth_date)
         
         usuario.save()  # Guarda el usuario en la base de datos
 
-        return redirect('/login')  # Redirige después de guardar el usuario
-    
+        # Autentica al usuario recién creado y realiza el login
+        user = authenticate(username=email, password=contraseña)
+        if user is not None:
+            login(request, user)
+            
+            # Agrega el mensaje de éxito
+            messages.success(request, 'Usuario registrado y logeado exitosamente.')
+
+            # Redirige a la página principal
+            return redirect('/')
+
     # Si el método no es POST, renderiza el formulario vacío
-    return render(request, 'crear_usuario.html')
+    return render(request, 'register.html')
 
 @login_required
 def cursos_view(request):
